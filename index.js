3c8813cbb617d325c1756215339e57f0bc03caa0
NicollasDavi <132391248+NicollasDavi@users.noreply.github.com>
2023-11-01 18:48:05 -0300

    quarto

--- index.js
+++ index.js
@@ -20,9 +20,37 @@ window.addEventListener('scroll', function() {
 
 function openIframe() {
     document.getElementById('iframeContainer').style.display = 'block';
+    
 }
 
 function closeIframe() {
     const iframeContainer = document.getElementById('iframeContainer');
     iframeContainer.style.display = 'none';
 }
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const cardHs = document.querySelectorAll('.cardH');
+
+    cardHs.forEach(function(cardH) {
+        const icon = cardH.querySelector('.icon');
+        const iconPurple = icon.querySelector('.purple');
+        const iconWhite = icon.querySelector('.white');
+        
+        cardH.addEventListener('mouseenter', function() {
+            iconPurple.style.opacity = 0;
+            iconWhite.style.opacity = 1;
+            iconWhite.style.display = 'inline-block';
+        });
+
+        cardH.addEventListener('mouseleave', function() {
+            iconPurple.style.opacity = 1;
+            iconWhite.style.opacity = 0;
+            iconWhite.style.display = 'none';
+        });
+    });
+});
+
+
