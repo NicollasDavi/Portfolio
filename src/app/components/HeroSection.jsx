4be1650aa22090946ca25dba15121972178ece32
NicollasDavi <132391248+NicollasDavi@users.noreply.github.com>
2024-02-28 00:30:06 -0300

    hidden

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -61,15 +61,16 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative sm:hidden md:hidden" >
-            <Image
-              src="/images/stk.png"
-              alt="hero image"
-              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              width={300}
-              height={300}
-            />
-          </div>
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative sm:hidden md:hidden">
+  <Image
+    src="/images/stk.png"
+    alt="hero image"
+    className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+    width={300}
+    height={300}
+  />
+</div>
+
         </motion.div>
       </div>
     </section>
